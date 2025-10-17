a54003ac9460e6eedd0a6ed1642d084fec2073e1
Long-pham2008 <phamhoanglong180108@gmail.com>
2025-10-17 21:46:29 +0000

    ajshvfvyci

--- Main.java
+++ Main.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
 
-public class Main
+public class Main 
 {
   public static void main(String[] args)
-  {
-    // write solutions to problems here
+   {
+    Scanner input = new Scanner(System.in);
+    int number;
+    int sum = 0;
+
+    System.out.println("Enter any non-negative numbers. Enter -1 to finish.");
+
+    while (true) 
+    {
+      System.out.print("Enter a number: ");
+      number = input.nextInt();
+
+      if (number == -1) 
+      {
+        break; 
+      }
+
+      if (number >= 0) 
+      {
+        sum += number; 
+      } 
+      else 
+      {
+        System.out.println("Negative numbers are not allowed (except -1 to stop).");
+      }
+    }
+
+    System.out.println("The total sum is: " + sum);
+    input.close();
   }
 }
