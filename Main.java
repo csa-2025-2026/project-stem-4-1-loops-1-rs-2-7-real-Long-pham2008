052513b5ee65c79bab4e3ab84247e967f916b4dd
Long-pham2008 <phamhoanglong180108@gmail.com>
2025-10-20 21:46:20 +0000

    long

--- Main.java
+++ Main.java
@@ -31,6 +31,28 @@ public class Main
     }
 
     System.out.println("The total sum is: " + sum);
+
+    // Problem 2
+    System.out.println("How many numbers do you want to input");
+    int N = input.nextInt();
+    int counter = 0;
+    int userInput;
+    int max = Integer.MIN_VALUE;
+    while (counter < N)
+    {
+      userInput = input.nextInt();
+      if (userInput > max)
+      {
+        max = userInput;
+      }
+      counter ++;
+    }
+    System.out.println("The max number is " + max);
     input.close();
+    
+    
+
+
   }
 }
+
